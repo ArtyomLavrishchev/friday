67d65feaeac4f1c195abe09ea83d53450b0b3fe7
Sergey Shaporov <sshaporov86@gmail.com>
2020-11-22 00:04:53 +0300

    added super components

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SuperButton from '../common/SuperButton/SuperButton'
+import SuperCheckbox from '../common/SuperCheckbox/SuperCheckbox'
+import SuperInputText from '../common/SuperInputText/SuperInputText'
 
 const Test = () => {
     return (
         <div>
-            Test
+            <SuperButton>Button</SuperButton>
+            <SuperCheckbox>Checkbox</SuperCheckbox>
+            <SuperInputText/>
         </div>
     )
 }
